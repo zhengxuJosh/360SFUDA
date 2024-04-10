7b67a8ac97bb7340e04a6b273d0958f87f9294c9
XuZheng <49426295+zhengxuJosh@users.noreply.github.com>
2024-04-10 10:28:36 +0800

    Update val_city.py

--- val_city.py
+++ val_city.py
@@ -27,33 +27,6 @@ from collections import OrderedDict
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_confusion_matrix(cm, class_names, save_path):
-    """
-    绘制混淆矩阵的热图，不包含数值标注，只显示颜色。
-    :param cm: 混淆矩阵
-    :param class_names: 类别名称列表
-    :param save_path: 图片保存路径
-    """
-    fig, ax = plt.subplots(figsize=(12, 12))
-    im = ax.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
-    ax.figure.colorbar(im, ax=ax)
-    
-    # 显示所有的刻度...
-    ax.set(xticks=np.arange(cm.shape[1]),
-           yticks=np.arange(cm.shape[0]),
-           # ...并且为它们标上类别名称
-           xticklabels=class_names, yticklabels=class_names,
-           title='Confusion Matrix',
-           ylabel='True label',
-           xlabel='Predicted label')
-
-    # 旋转刻度标签并设置其对齐方式
-    plt.setp(ax.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
-
-    fig.tight_layout()
-    plt.savefig(save_path, bbox_inches='tight')
-    # plt.show()
-
 def remove_module_prefix(state_dict):
     new_state_dict = OrderedDict()
     for key, value in state_dict.items():
@@ -73,12 +46,6 @@ class ToLabel:
         
         #return np.asarray(image, np.float32)
 
-def adjust_learning_rate_poly(optimizer, epoch, num_epochs, base_lr, power):
-    lr = base_lr * (1-epoch/num_epochs)**power
-    for param_group in optimizer.param_groups:
-        param_group['lr'] = lr
-    return lr
-
 def main():
     parser = argparse.ArgumentParser(description='pytorch implemention')
     parser.add_argument('--batch-size', type=int, default=6, metavar='N',
@@ -90,16 +57,6 @@ def main():
     parser.add_argument('--seed', type=int, default=1, metavar='S',
                         help='random seed (default: 1)')
     parser.add_argument('--local_rank', type=int)
-    parser.add_argument('--save_root', default = '',
-                        help='Please add your model save directory') 
-    parser.add_argument('--exp_name', default = '',
-                        help='')
-    parser.add_argument('--sup_set', type=str, default='train', help='supervised training set')
-    parser.add_argument('--cutmix', default =False, help='cutmix')
-    #================================hyper parameters================================#
-    parser.add_argument('--alpha', type=float, default =0.5, help='alpha')
-    parser.add_argument('--lamda', type=float, default =0.001, help='lamda')
-    #================================================================================#
     args = parser.parse_args()
 
     best_performance_pin = 0.0
@@ -115,46 +72,24 @@ def main():
             print('init lr: {}, batch size: {}, gpus:{}'.format(args.lr, args.batch_size, dist.get_world_size()))
 
         num_classes = 19
-        # Cityscapes dataset
-        # ------------------------------------------------------------------------------------------------------------#
-        img_mean=[0.485, 0.456, 0.406]
-        img_std=[0.229, 0.224, 0.225]
-        city_crop_size = 512
-        city_dataset_path = "/hpc2hdd/home/xzheng287/360Seg/TPAMI_360uda/dataset/city" # cityscapes dataset root
-        city_label_dataset = CityDataset(f'{city_dataset_path}',split='train', base_size=2048, crop_size=city_crop_size, norm_mean=img_mean, norm_std=img_std, pass_like=False)        
-        city_label_sampler = DistributedSampler(city_label_dataset, num_replicas=dist.get_world_size()) 
-        city_label_loader = torch.utils.data.DataLoader(city_label_dataset,batch_size=args.batch_size,sampler=city_label_sampler,num_workers=12,worker_init_fn=lambda x: random.seed(time.time() + x),drop_last=True,)
-        
-        # city_val_dataset = CityDataset(f'{city_dataset_path}', split='val', base_size=2048, crop_size=city_crop_size, norm_mean=img_mean, norm_std=img_std)
-        # val_loader = torch.utils.data.DataLoader(city_val_dataset,batch_size=args.batch_size,shuffle=False,num_workers=12)
+
         # DensePASS dataset
         # ------------------------------------------------------------------------------------------------------------#
         input_transform_cityscapes = Compose([ToTensor(),])
         target_transform_cityscapes = Compose([ToLabel(),])  
-        train_root = "/hpc2hdd/home/xzheng287/360Seg/data" # training set root
-        val_root = "/hpc2hdd/home/xzheng287/360Seg/data/DensePASS"   # validation set root
-        train_DensePASS = densepass_train(train_root, list_path='/hpc2hdd/home/xzheng287/360Seg/TPAMI_360uda/dataset/train.txt',set=None)
+        val_root = "/DensePASS"  
         val_DensePASS = densepass_val(val_root, input_transform=input_transform_cityscapes,target_transform=target_transform_cityscapes, target=True)
-        pass_train_sampler = DistributedSampler(train_DensePASS, num_replicas=dist.get_world_size())
-        pass_train_loader = torch.utils.data.DataLoader(train_DensePASS,batch_size=args.batch_size,sampler=pass_train_sampler,num_workers=12,worker_init_fn=lambda x: random.seed(time.time() + x),drop_last=True,)
         pass_val_loader = torch.utils.data.DataLoader(val_DensePASS,batch_size=1,shuffle=False,num_workers=12)
         model = Seg(backbone='mit_b2',num_classes=num_classes,embedding_dim=512,pretrained=True) 
-        pretrained_dict = torch.load('/hpc2hdd/home/xzheng287/360Seg/TPAMI_360uda/github/city_b2_52.99.pth', map_location='cpu')
+        pretrained_dict = torch.load('/city_b2_52.99.pth', map_location='cpu')
         pretrained_dict = remove_module_prefix(pretrained_dict)
 
         model.load_state_dict(pretrained_dict)
         
         model = model.to(args.local_rank)
-        # model = DistributedDataParallel(model,device_ids=[args.local_rank], output_device=args.local_rank, broadcast_buffers=False, find_unused_parameters=True)
-            
-        # print(f'syn_val Dataset length:{len(syn_val)};')
-        # print(f'dense_val Dataset length:{len(dense_val)};')
 
         with torch.no_grad():     
             model.eval()
-            # print('Stanford Pinhole Dataset')
-            # best_performance_pin = validation(num_classes, NAME_CLASSES, args.local_rank, stanford_syn_val_loader, model)
-            
             print('Panoramic Dataset')
             best_performance_pan = validation(num_classes, NAME_CLASSES, args.local_rank, pass_val_loader, model)
             
@@ -164,8 +99,6 @@ def validation(num_classes, NAME_CLASSES, device, testloader, model1):
     hist = np.zeros((num_classes, num_classes))
 
     for i, data in enumerate(testloader):
-        # if index % 100 == 0:
-        #     print ('%d processd' % index)
 
         [image, label, _, _] = data 
         image, label = image.to(device), label.to(device)
@@ -176,10 +109,6 @@ def validation(num_classes, NAME_CLASSES, device, testloader, model1):
 
         label = label.cpu().data[0].numpy()
         hist += fast_hist(label.flatten(), output.flatten(), num_classes)
-
-    class_names = NAME_CLASSES
-    plot_confusion_matrix(hist, class_names,
-                          save_path="/hpc2hdd/home/xzheng287/360Seg/TPAMI_360uda/visualization/confusion_matrix/confusion_matrix.png")
 
     mIoUs = per_class_iu(hist)
     for ind_class in range(num_classes):
